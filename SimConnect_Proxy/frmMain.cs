58de47687ea5817d97db710d4e6f529e38fa4339
draaagonlaird@msn.com <dragonlaird@msn.com>
2021-05-20 07:58:14 +0100

    Data now saved to a CSV file: "Documents\FS_Data_Stream.csv"

--- SimConnect_Proxy/frmMain.cs
+++ SimConnect_Proxy/frmMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -17,6 +18,9 @@ namespace SimConnect_Proxy
 
         private const int bytesPerDisplayLine = 8; // Number of bytes to display per line in both Data Sent and Data Received text boxes
         private const int maxLinesToDisplay = 1000; // Maximum number of lines per TextBox, older lines beyond this count are removed
+
+        private FileStream dataStream;
+        private const string dataFilePath = "FS_Data_Stream.csv";
 
         public frmMain()
         {
@@ -49,6 +53,8 @@ namespace SimConnect_Proxy
                 _proxy.Notifications += NotificationReceived;
                 NotificationReceived(this, new ProxyMessage { Message = "Starting Proxy" });
                 _proxy.StartListener(listenerAddress, listenerPort);
+                var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), dataFilePath);
+                dataStream = CreateFile(filePath);
                 ((Button)sender).Text = "Stop Proxy";
             }
             else
@@ -60,6 +66,42 @@ namespace SimConnect_Proxy
                 cbLocalConnected.Checked = false;
                 cbRemoteConnected.Checked = false;
                 ((Button)sender).Text = "Start Proxy";
+                if (dataStream != null)
+                    CloseFile();
+            }
+        }
+
+        private FileStream CreateFile(string filePath)
+        {
+            if (!string.IsNullOrEmpty(filePath) && !filePath.Any(x=> Path.InvalidPathChars.Contains(x)))
+            {
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
+                var fileStream = File.OpenWrite(filePath);
+                return fileStream;
+            }
+            else
+            {
+                NotificationReceived(this, new ProxyMessage { Message = $"Cannot create file {filePath}", Severity = 2 });
+                return null;
+            }
+        }
+
+        private void CloseFile()
+        {
+            if (dataStream != null)
+            {
+                dataStream.Flush();
+                dataStream.Close();
+            }
+        }
+
+        private void WriteToFile(string text)
+        {
+            if (dataStream != null)
+            {
+                var bytes = Encoding.UTF8.GetBytes(text);
+                dataStream.Write(bytes, 0, bytes.Length);
             }
         }
 
@@ -126,27 +168,44 @@ namespace SimConnect_Proxy
                 return;
             }
             StringBuilder sb = new StringBuilder();
+            StringBuilder op = new StringBuilder(); // Used to generate the CSV file output
+            op.Append(txtBox == txtDataSent ? "L," : "R,");
+            op.Append("\"0000\",");
             sb.AppendLine($"[{DateTime.Now:HH:mm:ss}]");
             string displayChars = string.Empty;
+            sb.Append("0000: ");
             for(var i = 0; i < data.Length; i++)
             {
                 var byt = data[i];
                 sb.Append($"{byt:X2} ");
+                op.Append($"\"0x{byt:X2}\",");
                 displayChars += DisplayChar(byt) + " ";
                 if (displayChars.Length > 0 && (displayChars.Length / 2) % bytesPerDisplayLine == 0)
                 {
                     sb.Append(" " + displayChars + "\r\n");
+                    op.Append($"\"{displayChars}\"\r\n");
                     displayChars = string.Empty;
+                    if (i < data.Length - 1)
+                    {
+                        sb.Append((i + 1).ToString("0000") + ": ");
+                        op.Append(txtBox == txtDataSent ? "L," : "R,");
+                        op.Append("\"" + (i + 1).ToString("0000") + "\",");
+                    }
                 }
             }
             if (displayChars.Length != 0)
             {
                 var result = sb.ToString();
-                result = result.PadRight(1 + 3 * (data.Length % bytesPerDisplayLine), ' ');
+                result = result.PadRight(result.Length + 1 + 3 * (data.Length % bytesPerDisplayLine), ' ');
                 sb = new StringBuilder(result);
                 sb.Append(displayChars + "\r\n");
+                result = op.ToString();
+                result = result.PadRight(result.Length + data.Length % bytesPerDisplayLine, ',');
+                op = new StringBuilder(result);
+                op.Append($"\"{displayChars}\"\r\n");
             }
             txtBox.Text += sb.ToString();
+            WriteToFile(op.ToString());
             while (txtBox.Lines.Count() > maxLinesToDisplay)
             {
                 txtBox.Lines = txtBox.Lines.Skip(1).Take(txtBox.Lines.Length - 1).ToArray();
